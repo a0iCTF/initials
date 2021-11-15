02a89e0ee94f764d12ee2e3ed6cd4090cd2b4b15
Kieran <85199718+realkieranlul@users.noreply.github.com>
2021-11-15 19:40:43 +0000

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,10 @@
+#Kieran Wallace
+#KEKW 
+
+print("K      K     W       W")
+print("K     K      W       W")
+print("K   K        W       W")
+print("K K          W   W   W")
+print("K   K        W W   W W")
+print("K     K      W W   W W")
+print("K       K    W       W")
